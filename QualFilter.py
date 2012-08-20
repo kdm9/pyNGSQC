e7f1f3463bddbd9bb7bcda3f905924f2ff8a25db
k.d.murray.91 <k.d.murray.91@gmail.com>
2012-08-20 22:47:09 +1000

    getting pyNGSQC base and util classes working, see test.py

--- QualFilter.py
+++ QualFilter.py
@@ -1,7 +1,5 @@
-from sys import stderr 
-import gzip
-import bz2
 import pyNGSQC
+
 
 class QualityFilter(pyNGSQC.NGSQC):
     """
@@ -14,40 +12,37 @@ class QualityFilter(pyNGSQC.NGSQC):
             out_file_name (str): path of output file, can be .fastq, .fastq.gz
                 or .fastq.bz2
             qual_threshold (int): minimum "pass" phred score
-            pass_rate (float): minimum fraction of 
+            pass_rate (float): minimum fraction of
     """
-    
+
     def __init__(self, in_file_name, out_file_name, qual_threshold=15,
                   pass_rate=0.9, max_Ns=-1, qual_offset=64,
                   compression=pyNGSQC.GUESS_COMPRESSION):
-        
-        super(QualityFilter, self).__init__(
+        self.in_file = pyNGSQC.FastqReader(
                                             in_file_name,
-                                            out_file_name,
-                                            qual_offset=qual_offset,
                                             compression=compression
-                                           )
-        
+                                          )
+
         self.pass_rate = float(pass_rate)
         self.qual_threshold = qual_threshold
         self.max_Ns = max_Ns
-    
-    def _passes_score_qc(self,read):
+
+    def _passes_score_qc(self, read):
         qual = read[3]
         read_len = len(qual)
         low_scores = 0
         for p in qual:
             if self.get_qual_from_phred(p) < self.qual_threshold:
                 low_scores += 1
-        this_pass_rate = 1.0 - float(low_scores)/float(read_len)
+        this_pass_rate = 1.0 - float(low_scores) / float(read_len)
         if this_pass_rate < self.pass_rate:
             return False
         else:
             return True
-            
+
     def filter_read(self, read):
         # the and max_Ns >= 0 skips the N-check if not required
-        if self.max_Ns >= 0 and int(self._num_Ns_in_read(read)) > self.max_Ns :
+        if self.max_Ns >= 0 and int(self._num_Ns_in_read(read)) > self.max_Ns:
             self.num_bad_reads += 1
             return False
         elif self._passes_score_qc(read):
@@ -56,11 +51,11 @@ class QualityFilter(pyNGSQC.NGSQC):
         else:
             self.num_good_reads += 1
             return True
-    
+
     def process_read(self, this_read):
         if self.filter_read(this_read):
                     self.writer.write(this_read)
-    
+
     def run(self):
         self._run(self.process_read, self.print_summary)
 
@@ -69,6 +64,6 @@ if __name__ == "__main__":
     qf = QualityFilter(sys.argv[1],
                        sys.argv[2],
                        compression=int(sys.argv[3]),
-                       pass_rate=1.0, 
+                       pass_rate=1.0,
                        qual_offset=33)
     qf.run()
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import pyNGSQC as ngs
+in_file = "/home/kevin/UniWork/BIOL3157/Assignments/2/in.fastq"
+out_file = "/home/kevin/UniWork/BIOL3157/Assignments/2/out.fastq"
+def test_1():
 fqrdr = ngs.FastqReader(
-    "/home/kevin/UniWork/BIOL3157/Assignments/2/in.fastq",
-    compression=ngs.NO_COMPRESSION
+    "/home/kevin/UniWork/BIOL3157/Assignments/2/in.fastq"
     )
 print fqrdr
 for read in fqrdr:
