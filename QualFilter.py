a8bfc51f5babac975abea1f0b0a0d63ac26d913c
k.d.murray.91 <k.d.murray.91@gmail.com>
2012-08-20 23:26:56 +1000

    getting pyNGSQC base and util classes working, see test.py

--- QualFilter.py
+++ QualFilter.py
@@ -20,45 +20,56 @@ class QualFilter(pyNGSQC.NGSQC):
     def __init__(self, in_file_name, out_file_name, qual_threshold=15,
                   pass_rate=0.9, max_Ns=-1, qual_offset=64,
                   compression=pyNGSQC.GUESS_COMPRESSION):
+        self.in_file_name = in_file_name
+        self.out_file_name = out_file_name
         self.reader = pyNGSQC.FastqReader(
-                                           in_file_name,
+                                           self.in_file_name,
                                            compression=compression
                                          )
         self.writer = pyNGSQC.FastqWriter(
-                                           out_file_name,
+                                           self.out_file_name,
                                            compression=compression
                                           )
         self.pass_rate = float(pass_rate)
         self.qual_threshold = qual_threshold
+        self.qual_offset = qual_offset
         self.max_Ns = max_Ns
+        self.num_reads = 0
+        self.num_good_reads = 0
+        self.num_bad_reads = 0
 
     def _passes_score_qc(self, read):
         qual = read[3]
         read_len = len(qual)
         low_scores = 0
         for p in qual:
-            if self.get_qual_from_phred(p) < self.qual_threshold:
+            if self._get_qual_from_phred(p) < self.qual_threshold:
                 low_scores += 1
         this_pass_rate = 1.0 - float(low_scores) / float(read_len)
-        if this_pass_rate < self.pass_rate:
+        print this_pass_rate, self.pass_rate, this_pass_rate <= self.pass_rate
+        if this_pass_rate <= self.pass_rate:
             return False
         else:
             return True
 
     def filter_read(self, read):
-        # the and max_Ns >= 0 skips the N-check if not required
-        if self.max_Ns >= 0 and int(self._num_Ns_in_read(read)) > self.max_Ns:
+        if self.max_Ns != -1 and\
+         int(self._num_Ns_in_read(read)) > self.max_Ns:
             self.num_bad_reads += 1
+            print "too many Ns"
             return False
-        elif self._passes_score_qc(read):
+        elif not self._passes_score_qc(read):
             self.num_bad_reads += 1
+            print "bad score"
             return False
         else:
+            print "good read"
             self.num_good_reads += 1
             return True
 
     def print_summary(self):
         stderr.write("QC check finished:\n")
+        stderr.write("Processed %i reads\n" % self.num_reads)
         stderr.write(
                 "\t%i sequences passed QC, wrote them to %s\n" %
                 (self.num_good_reads, self.out_file_name)
@@ -70,17 +81,15 @@ class QualFilter(pyNGSQC.NGSQC):
 
     def run(self):
         for read in self.reader:
+            self.num_reads += 1
             if self.filter_read(read):
                 self.writer.write(read)
-                self.good_reads += 1
-            else:
-                self.bad_read += 1
         self.print_summary()
 
 
 if __name__ == "__main__":
     import sys
-    qf = QualityFilter(sys.argv[1],
+    qf = QualFilter(sys.argv[1],
                        sys.argv[2],
                        compression=int(sys.argv[3]),
                        pass_rate=1.0,
--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 import pyNGSQC as ngs
 import QualFilter as qfil
-in_file = "/home/kevin/UniWork/BIOL3157/Assignments/2/in.fastq"
+in_file = "/home/kevin/UniWork/BIOL3157/Assignments/2/big.fastq"
 out_file = "/home/kevin/UniWork/BIOL3157/Assignments/2/out.fastq"
 
 
 def test_1():
     fqrdr = ngs.FastqReader(in_file)
-    print fqrdr
     count = 0
     for read in fqrdr:
         count += 1
@@ -16,9 +15,7 @@ print "Test 1 passed"
 
 
 def test_2():
-    qf = qfil.QualFilter(in_file, out_file)
-    print qf
+    qf = qfil.QualFilter(in_file, out_file, qual_threshold=20)
     qf.run()
 test_2()
 print "Test 2 passed"
-
