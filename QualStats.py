f881c71a2ee5a64eb228aa73bad096fb787e0ccd
Kevin Murray <k.d.murray.91@gmail.com>
2012-08-24 19:33:37 +1000

    Working on QualStats still, TODO: implement fastx_quality_stats style output. perhaps output type should be a option to the class, either fastx, csv, json, or others

--- QualStats.py
+++ QualStats.py
@@ -30,16 +30,10 @@ class QualStats(pyNGSQC.NGSQC):
         # are integers, saves mem
         self.initial_score_counts = [0 for iii in xrange(self.qual_offset, 126)]
 
-    def print_output(self):
-        stderr.write("QualTrimmer finished:\n")
-        stderr.write(
-                      "\t%i sequences passed QC, wrote them to %s\n" %
-                      (self.num_good_reads, self.out_file_name)
-                    )
-        stderr.write(
-                      "\t%i sequences failed QC, and were ignored\n" %
-                      self.num_bad_reads,
-                    )
+    def print_summary(self):
+        import json
+        print json.dumps(self.position_scores, indent=2)
+        print json.dumps(self.position_bases, indent=2)
 
     def process_read(self, read):
         while len(self.position_bases) < 101:  # len(read[1]):
@@ -60,17 +54,9 @@ class QualStats(pyNGSQC.NGSQC):
         for read in self.reader:
             self.num_reads += 1
             self.process_read(read)
-        import json
-        print json.dumps(self.position_scores, indent=2)
-        print json.dumps(self.position_bases, indent=2)
         self.print_summary()
         return True
 
 
 qs = QualStats("/home/kevin/UniWork/BIOL3157/Assignments/2/U4852380.txt")
 qs.run()
-for i in dir(qs):
-    if i[0] != "_":
-        mem = getattr(qs, i)
-        if hasattr(mem, "__len__"):
-            print i, len(mem)
