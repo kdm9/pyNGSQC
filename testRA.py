68115c60e7b1f725aa55477123aed23c1752e412
Kevin Murray <k.d.murray.91@gmail.com>
2012-09-09 22:33:28 +1000

    Fixed git includes, test files now removed

--- testRA.py
+++
@@ -1,8 +0,0 @@
-
-##MEMORY LEAK IN LARGE FILES
-import pyNGSQC
-in_name = "/home/kevin/workspace/pel_huge.txt"
-ra = pyNGSQC.FastqRandomAccess(in_name)
-print len(ra.record_positions)
-#for iii in xrange(len(ra.record_positions)):
-#    print iii, ra.get(iii)[0]
