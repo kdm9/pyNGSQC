51bb40dfda879fe61fefbaefeae771cffdd3d049
Kevin Murray <k.d.murray.91@gmail.com>
2012-09-11 15:31:06 +1000

    Collapser: removed file size limit and increased key size, improved tmp file handling, implemented summary printer

--- Collapser.py
+++ Collapser.py
@@ -13,41 +13,51 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import pyNGSQC
+import sys
+from tempfile import NamedTemporaryFile as namedtmp
 
 
 class Collapser(pyNGSQC.NGSQC):
-    MAX_FILE_SIZE = 2 << 30  # 2GB
+    #MAX_FILE_SIZE = 2 << 30  # 2GB
 
     def __init__(
                  self,
                  in_file_name,
                  out_file_name,
-                 key_length=4,
+                 key_length=6,
                  compression=pyNGSQC.GUESS_COMPRESSION,
                  verbose=False
                 ):
         self.in_file_name = in_file_name
         self.out_file_name = out_file_name
         self.reader = pyNGSQC.FastqReader(
-                                           self.in_file_name,
-                                           compression=compression
-                                         )
+            self.in_file_name,
+            compression=compression
+            )
         self.verbose = verbose
         self.key_length = key_length
         self.keys = []
         self.num_reads = 0L
-        self.tmp_file_name = "%s.tmp"
+        self.num_non_unique_reads = 0
+        self.num_unique_reads = 0
+        self.tmp_file_names = {}
 
     def _split_files(self):
         file_sizes = {}
         sub_file_handles = {}
         for read in self.reader:
             key = read[1][:self.key_length]
-            file_name = self.tmp_file_name % key
+            # None means guess tmp dir
             if key not in self.keys:
                 self.keys.append(key)
                 # If in keys, file handle should exist
-                sub_file_handles[key] = open(file_name, "wb")
+                sub_file_handles[key] = namedtmp(
+                    mode="wb",
+                    prefix=key + "_",
+                    delete=False
+                    )
+                file_name = sub_file_handles[key].name
+                self.tmp_file_names[key] = file_name
             read_str = "\n".join(read)
             sub_file_handles[key].write(read_str + "\n")
 
@@ -57,29 +67,6 @@ class Collapser(pyNGSQC.NGSQC):
             this_file_size = sub_file_handles[key].tell()
             file_sizes[key] = this_file_size
             sub_file_handles[key].close
-
-#        # Not sure if this works, needs debugging
-#        while max(file_sizes.values()) > self.MAX_FILE_SIZE:
-#            self.key_length += 1
-#            for parent_key in self.keys:
-#                this_read = []
-#                parent_reader = pyNGSQC.FastqReader(in_file_name)
-#                sub_file_handles[parent_key].close()
-#                for line in self.sub_files[parent_key]:
-#                    this_read.append(line)
-#                    if len(this_read) == 4:
-#                        this_key = this_read[1][:self.key_length]
-#                        file_name = "collaper_sort_%s" % this_key
-#                        if this_key not in self.sub_files:
-#                            self.sub_files[this_key] = open(file_name, "wb")
-#                        read_str = "\n".join(this_read)
-#                        self.sub_files[this_key].write(read_str + "\n")
-#                sub_file_handles[key].seek(0)
-#                this_file_size = sub_file_handles[key].tell()
-#                file_sizes[key] = this_file_size
-#                del file_sizes[parent_key]
-#                sub_file_handles[parent_key].close()
-#                del sub_file_handles[parent_key]
 
     def _read_to_tuple(self, read):
         return (read[1], read[0], read[3], read[2])
@@ -93,33 +80,39 @@ class Collapser(pyNGSQC.NGSQC):
         return read
 
     def _sort(self):
+        sorted_writer = pyNGSQC.FastqWriter(self.out_file_name)
         for key in sorted(self.keys):
             these_reads = []
-            file_name = self.tmp_file_name % key
+            file_name = self.tmp_file_names[key]
 
             reader = pyNGSQC.FastqReader(file_name)
             for read in reader:
                 these_reads.append(self._read_to_tuple(read))
             these_reads.sort()
+            self.num_reads += reader.num_reads
             reader.close()
 
-            sorted_writer = pyNGSQC.FastqWriter("sorted_" + file_name)
             last_read_seq = ""
             for read_tuple in these_reads:
+
                 if read_tuple[0] != last_read_seq:
-                    sorted_writer.write(self._tuple_to_read(read_tuple))
                     last_read_seq = read_tuple[0]
+                    self.num_unique_reads += 1
+                    sorted_writer.write(self._tuple_to_read(read_tuple))
+                else:
+                    self.num_non_unique_reads += 1
+        for filename in self.tmp_file_names:
+            os.remove(filename)
 
     def print_summary(self):
-        pass
+        sys.stderr.write("Collapser finished\n")
+        sys.stderr.write("\tAnalysed %i reads\n" % self.num_reads)
+        sys.stderr.write("\tFound %i unique reads\n" % self.num_unique_reads)
+        sys.stderr.write("\tRemoved %i non-unique reads\n" % \
+         self.num_non_unique_reads)
 
     def run(self):
         self._split_files()
         self._sort()
+        self.print_summary()
         return True
-fldr = "/home/kevin/workspace/"  # /var/ws/borevitz/
-cl = Collapser(
-               fldr + "in.fastq",  # pel_1.small.fastq
-               fldr + "test.out.fastq"
-              )
-cl.run()
